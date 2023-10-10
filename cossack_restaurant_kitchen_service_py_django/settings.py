cf554edfd57b4e2a8527733bc8df7f2fc4bf10d6
Andrii Kurta <andriykurta97@gmail,com>
2023-10-10 12:32:01 +0200

    Fix settings.py file (delete wrong app in installed aps)

--- cossack_restaurant_kitchen_service_py_django/settings.py
+++ cossack_restaurant_kitchen_service_py_django/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ["127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
-    'render.apps.RenderConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
